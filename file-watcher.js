580df2e24c25bc911f2ba4dda44a4a79835f10a6
Rob W <robpwatkins@gmail.com>
2020-01-30 14:00:11 -0600

    here we go.

---
+++ file-watcher.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const md5 = require('md5');
+require('log-timestamp');
+
+const buttonPressesLogFile = './button-presses.log';
+
+console.log(`Watching for file changes on ${buttonPressesLogFile}`);
+
+let fsWait = false;
+
+fs.watch(buttonPressesLogFile, (event, filename) => {
+  if (filename) {
+    if (fsWait) return;
+    fsWait = setTimeout(() => {
+      fsWait = false;
+    }, 100);
+    console.log(`${filename} file changed`);
+  }
+});
